395bea184d477ea2fa26d774aba5b32b0549f566
Stephanie <me@stephaniemariewatson.com>
2021-11-04 21:19:12 -0400

    Add project files.

---
+++ MovieAPI/Data/MovieAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieAPI.Models;
+
+namespace MovieAPI.Data
+{
+    public class MovieAPIContext : DbContext
+    {
+        public MovieAPIContext (DbContextOptions<MovieAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieAPI.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MovieAPI/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Genre { get; set; }
+        [Required]
+        public double Runtime { get; set; }
+    }
+}
